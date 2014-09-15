5609886074a84ce420abaf2ebea9281fcae6f6a6
Oleksandr Povshyk <xtwinpeaks@gmail.com>
2014-09-16 01:23:14 +0300

    add title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def form_title(page_title)
+
+    base_title = "blmt"
+    if page_title.empty?
+      base_title
+    else
+      base_title + " | " + page_title
+    end
+
+  end
+
 end
